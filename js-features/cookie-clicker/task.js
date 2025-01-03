649482f7a052d011f23f8b5afa7622a8cda4c326
QeeeXXX <zakharchenkoserg297@gmail.com>
2025-01-03 13:46:49 +0300

    Update task.js

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,34 +1,10 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const cookie = document.querySelector('.cookie');
-    const counter = document.getElementById('clicks');
-    const speedCounter = document.getElementById('click-speed');
+document.addEventListener("DOMContentLoaded", function() {
+    const cookie = document.getElementById('cookie');
+    const counter = document.getElementById('clicker__counter');
     let clicks = 0;
-    let lastClickTime = null;
-    let averageClicksPerSecond = 0;
 
-    cookie.addEventListener('click', function () {
+    cookie.addEventListener('click', function() {
         clicks++;
         counter.textContent = clicks;
-
-        // Чередование уменьшения и увеличения размера печенья
-        if (this.style.width === '150px') {
-            this.style.width = '250px';
-            this.style.height = '250px';
-        } else {
-            this.style.width = '150px';
-            this.style.height = '150px';
-        }
-
-        // Расчёт средней скорости кликов
-        const currentTime = new Date().getTime();
-        if (lastClickTime !== null) {
-            const elapsedTime = (currentTime - lastClickTime) / 1000;
-            averageClicksPerSecond = 1 / elapsedTime;
-            speedCounter.textContent = averageClicksPerSecond.toFixed(2);
-        }
-        lastClickTime = currentTime;
-
-        // Показ блока со скоростью кликов
-        document.querySelector('.speed').style.display = 'block';
     });
 });
